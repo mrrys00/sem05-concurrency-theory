c184b5a9250ed739e7780fe3266046cca3efa64d
Szymon Ryś <rysszymon00@gmail.com>
2022-10-21 21:26:31 +0200

    add finished task 2

--- Lab02/CounterSemaphore.java
+++ Lab02/CounterSemaphore.java
@@ -1,26 +1,39 @@
 public class CounterSemaphore {
     private int counter;
+    private int max;
     // https://www.infoworld.com/article/2077413/semaphore.html
     
+    // max ilość koszyków i jeśli spadnie poniżej 0 to wait
     public CounterSemaphore() {
-        this(0);
+        this(10);
     }
-    public CounterSemaphore(int i) {
-        if (i < 0) throw new IllegalArgumentException(i + " < 0");
-        counter = i;
+    public CounterSemaphore(int max) {
+        this.counter = 0;
+        if (max <= 0) throw new IllegalArgumentException(max + " < 0");
+        this.max = max;
     }
     
-    public synchronized void release() {
-        if (counter == 0) {
-            this.notify();
+    public synchronized void release() {        // cart taken
+        while (counter == max) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
+        this.notifyAll();
         counter++;
     }
 
-    public synchronized void acquire() throws InterruptedException {
+    public synchronized void acquire() {        // cart put
         while (counter == 0) {
-            this.wait();
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
+        this.notifyAll();
         counter--;
     }
 }
