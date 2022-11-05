bdee0cf1885c86099e7b656a63c2d92d860d3a44
Szymon Ryś <rysszymon00@gmail.com>
2022-11-05 21:42:39 +0100

    add almost working lab03 zad3

--- Lab03/Zad3/WaiterMonitor.java
+++ Lab03/Zad3/WaiterMonitor.java
@@ -8,6 +8,8 @@ public class WaiterMonitor {
     final static PairsMonitor[] clientsReservations = new PairsMonitor[100]; // max 100 pairs
     private static RestaurantClient client1;
     private static RestaurantClient client2;
+    private long clientThreadID1;
+    private long clientThreadID2;
     private int max;
 
     final private Lock lock = new ReentrantLock();
@@ -22,27 +24,31 @@ public class WaiterMonitor {
     }
 
     public void reserve(RestaurantClient restaurantClient) throws InterruptedException {
-        if (clientsReservations[(int) restaurantClient.getID()].getWaiting() == null) {
-            System.out.println("Client " + (int) restaurantClient.getID() + " try to reserve table");
-            clientsReservations[(int) restaurantClient.getID()].setWaiting(restaurantClient);
-        } else if (clientsReservations[(int) restaurantClient.getID()].getWaiting() != null) {
-            System.out.println("Clients pair " + (int) restaurantClient.getID() + " reserved table");
-            client1 = clientsReservations[(int) restaurantClient.getID()].getWaiting();
-            clientsReservations[(int) restaurantClient.getID()].unsetWaiting();
-            client2 = restaurantClient;
-            // notEmpty.signal();
+        if (clientsReservations[restaurantClient.getID()].getWaiting() == null) {
+            System.out.println("Client " + restaurantClient.getID() + " try to reserve table");
+            clientsReservations[restaurantClient.getID()].setWaiting(restaurantClient);
+        } else if (clientsReservations[restaurantClient.getID()].getWaiting() != null) {
+            // while (client1 != null || client2 != null)
+            //     notEmpty.await();
 
+            /// TUTAJ POTRZEBA MECHANIZMU CZEKAJĄCEGO JEŚLI DWÓCH POPRZEDNICH KLIENTÓW NIE ZWOLNIŁO STOLIKA
+
+            System.out.println("Clients pair " + restaurantClient.getID() + " reserved table");
+            client1 = clientsReservations[restaurantClient.getID()].getWaiting();
+            clientsReservations[restaurantClient.getID()].unsetWaiting();
+            client2 = restaurantClient;
+            clientThreadID1 = client1.getId();
+            clientThreadID2 = client2.getId();
         }
 
     }
 
     public void release(RestaurantClient restaurantClient) throws InterruptedException {
-        System.out.println("Clients pair " + (int) restaurantClient.getID() + " release table");
-
-        WaiterMonitor.client1 = null;
-        WaiterMonitor.client2 = null;
-
-        // while (client1 != null && client2 != null)
-        //     notEmpty.await();
+        System.out.println("Clients pair " + restaurantClient.getID() + " release table ; thread " + restaurantClient.getId());
+        
+        if (restaurantClient.getId() == clientThreadID1)
+            WaiterMonitor.client1 = null;
+        else if (restaurantClient.getId() == clientThreadID2)
+            WaiterMonitor.client2 = null;
     }
 }
