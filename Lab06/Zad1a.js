d8df2b0a5e5c7292438487376e2c56d4fde25023
Szymon Ryś <rysszymon00@gmail.com>
2022-11-26 19:34:10 +0100

    add verified zad1a

--- Lab06/Zad1a.js
+++ Lab06/Zad1a.js
@@ -8,39 +8,42 @@ function printAsync(s, cb) {
 }
 
 function task1(cb, iter) {
+    if (iter == 0) {
+        console.log('done!');
+        return
+    }
     printAsync("1", function() {
-        task2(cb);
+        task2(cb, iter);
     });
 }
 
-function task2(cb) {
+function task2(cb, iter) {
     printAsync("2", function() {
-        task3(cb);
+        task3(cb, iter);
     });
 }
 
-function task3(cb) {
-    printAsync("3", cb);
+function task3(cb, iter) {
+    printAsync("3", function() {
+        task1(cb, iter-1);
+    });
 }
  
-//  // wywolanie sekwencji zadan
-//  task1(function() {
-//      console.log('done!');
-//  });
+// wywolanie sekwencji zadan
+// task1(function() {
+//     console.log('done!');
+// }, 4);
  
- 
-function loop(n) {
-    task1(null, n)
-    console.log('done!');
+/* 
+** Zadanie:
+** Napisz funkcje loop(n), ktora powoduje wykonanie powyzszej 
+** sekwencji zadan n razy. Czyli: 1 2 3 1 2 3 1 2 3 ... done
+** 
+*/
+ function loop(n) {
+    task1(function() {
+        console.log('done!');
+    }, n);
 };
 
 loop(4);
-
- /* 
- ** Zadanie:
- ** Napisz funkcje loop(n), ktora powoduje wykonanie powyzszej 
- ** sekwencji zadan n razy. Czyli: 1 2 3 1 2 3 1 2 3 ... done
- ** 
- */
- 
- // loop(4);
