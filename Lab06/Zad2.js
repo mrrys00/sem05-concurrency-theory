09d58dc6bd09f0966fe3bf27e85bdbe4fce38621
Szymon Ryś <rysszymon00@gmail.com>
2022-11-27 13:32:04 +0100

    add finished zad2

--- Lab06/Zad2.js
+++ Lab06/Zad2.js
@@ -1,6 +1,6 @@
 
 function printAsync(s, cb) {
-    var delay = Math.floor((Math.random() * 100) + 50);
+    var delay = Math.floor((Math.random() * 1000) + 500);
     setTimeout(function () {
         console.log(s);
         if (cb) {
@@ -18,18 +18,31 @@ function printAsync(s, cb) {
 
 
 function inparallel(parallel_functions, final_function) {
+    // solution 1.
+    let cnt = 0;
+    let arrLen = parallel_functions.length;
+    parallel_functions.forEach(cb => {
+        cb(() => {
+            cnt ++;
+            if (cnt == arrLen-1) final_function();
+        })
+    });
+
+    // solutin 2. - bad
     // for (let i = 0; i < parallel_functions.length-1; i++) {
     //     parallel_functions[i]();
     // }
     // parallel_functions[parallel_functions.length-1](() => D(() => {}))
-    myImparallel(parallel_functions, final_function, 0)
+    
+    // solution 3. - recursive
+    // myImparallel(parallel_functions, final_function, 0)
 }
 
 function myImparallel(parallel_functions, final_function, idx) {
     if (idx == parallel_functions.length) {
-        final_function()
+        final_function();
     } else {
-        parallel_functions[idx](() => myImparallel(parallel_functions, final_function, idx+1))
+        parallel_functions[idx](() => myImparallel(parallel_functions, final_function, idx+1));
     }
 }
 
@@ -38,7 +51,7 @@ const B = function (cb) { printAsync("B", cb); }
 const C = function (cb) { printAsync("C", cb); }
 const D = function (cb) { printAsync("Done", cb); }
 
-inparallel([A, B, C, A, B, C, A, B, C, A, B, C], D)
+inparallel([A, B, C, A, B, C, A, B, C, A, B, C], D);
 // A(() => {B(() => {C(() => {D(() => {})})})})
 
 // kolejnosc: A, B, C - dowolna, na koncu zawsze "Done" 
